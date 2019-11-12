8d4ccbdf717c1ed4a22017d2d41e12b797af2926
prattya datta <pratt@LAPTOP-500LE5P5.(none)>
2019-11-12 11:14:26 +0100

    testing web-responsiveness

--- index_finder.php
+++ index_finder.php
@@ -1,11 +1,23 @@
 <?php
-
-$file_name="C:\Users\pratt\Documents\finder-site\countries.csv";
-
-if (($handle=fopen($file_name,"r")) !== FALSE) {
-    while (($data=fgetcsv($handle,0,",")) != FALSE) {
-        echo $data[1];
-    }
-    $fclose($handle);
+$the_big_array=[];
+$file_name="countries.csv";
+if (($handle = fopen($file_name, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+       $the_big_array[]=$data;
 }
+    fclose($handle);
+}
+/*
+echo "<pre>";
+var_dump($the_big_array[245][3]);
+echo "</pre>";*/
+$columns = $the_big_array[0];
+for ($i=1; $i<count($the_big_array)-1; $i++){
+    foreach ($columns as $column_index => $column){
+        @$obj[$i]->$column = $the_big_array[$i][$column_index];
+    }
+}
+$json = json_encode($obj);
+/*echo $json;*/
+include 'formulario.php';
 ?>
